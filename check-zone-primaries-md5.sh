e03aca414a8f6c969f75105f2394f3e96fdbf12e
awkto <awkto@outlook.com>
2022-02-22 19:13:09 -0500

    added more scripts for domain migration and validation

--- check-zone-primaries-md5.sh
+++ check-zone-primaries-md5.sh
@@ -9,12 +9,15 @@ domainID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$doma
   -H "Content-Type: application/json" |jq .result[].id -r) \
 
 #check which primary masters are linked to zone \
-echo "Masters for domain : "$domain
-curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$domainID/secondary_dns" \
-	-H "X-Auth-Email: $CF_API_EMAIL" \
-	-H "X-Auth-Key: $CF_API_KEY" \
-	-H "Content-Type: application/json" \
-	| jq '.result.primaries[]' \
+echo "Masters MD5 is : "$(
+	curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$domainID/secondary_dns" \
+		-H "X-Auth-Email: $CF_API_EMAIL" \
+		-H "X-Auth-Key: $CF_API_KEY" \
+		-H "Content-Type: application/json" \
+		| jq '.result.primaries[]' \
+		| md5sum \
+	) \
+	" for domain "$domain
 
 
 #		| jq '.result | {name,primaries}'
--- loop-check-zone-primaries-md5.sh
+++ loop-check-zone-primaries-md5.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # NOTE : This script expects an argument, which should be a filename that contains a list of domains
 #        Each domain in that list will be processed one at a time
-domainlist=$1
 
-for domain in $(cat $domainlist);do \
-	echo "Domain Plan is "$(./check-zone-plan.sh $domain | jq .plan.name)" for domain : "$domain; \
+for i in $(cat $1);do \
+	./check-zone-primaries-md5.sh $i ; \
 done
