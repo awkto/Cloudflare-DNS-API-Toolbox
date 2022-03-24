96dddae1b53b9daa017dabdcd5bf59503908beca
awkto <awkto@outlook.com>
2022-03-24 12:23:34 -0400

    created a loop script for the verification script, for running against a list of domains

---
+++ loop-start-verification-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# NOTE : This script expects an argument, which should be a filename that contains a list of domains
+#        Each domain in that list will be processed one at a time
+
+domainlist=$1
+
+for domain in $(cat $domainlist);do \
+	./start-zone-activation-check.sh $domain; \
+done
+
