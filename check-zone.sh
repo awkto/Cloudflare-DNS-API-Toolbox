b860bc04820d06f9d189ff743cd773ecb8edab55
awkto <awkto@outlook.com>
2022-03-24 12:21:00 -0400

    converted argument to variable for cleaner code

--- check-zone.sh
+++ check-zone.sh
@@ -2,8 +2,10 @@
 export CF_API_EMAIL=$(cat .cloudflare-id)
 export CF_API_KEY=$(cat .cloudflare-token)
 
+domain=$1
+
 #Fetch the JSON of the entire zone, and filter out name, type, plan.name, plan.id
-curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$1" \
+curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$domain" \
      -H "X-Auth-Key: $CF_API_KEY"  \
      -H "X-Auth-Email: $CF_API_EMAIL" \
      -H "Content-Type: application/json" \
