78d26b1401ecf18bdf7d090399d861e996beabd5
awkto <awkto@outlook.com>
2022-04-27 23:17:01 -0400

    converted account ID to variable. de-coupled from code

--- add-zone-as-primary.sh
+++ add-zone-as-primary.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export CF_API_EMAIL=$(cat .cloudflare-id)
 export CF_API_KEY=$(cat .cloudflare-token)
+export CF_ACCOUNT_ID=$(cat .accountid)
 
 #note : this adds the new domain as Primary type, under FREE PLAN
 curl -s -X POST "https://api.cloudflare.com/client/v4/zones" \
 	-H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" \
 	-H "Content-Type: application/json" \
-	--data '{"account": {"id": "2099d679a66900a90bd05fbf0faf5469"}, "name":"'$1'","type":"full","plan": {"id": "0feeeeeeeeeeeeeeeeeeeeeeeeeeeeee", "name": "free"}}' \
+	--data '{"account": {"id": "'$CF_ACCOUNT_ID'"}, "name":"'$1'","type":"full","plan": {"id": "0feeeeeeeeeeeeeeeeeeeeeeeeeeeeee", "name": "free"}}' \
 	| jq .
-#    | jq .success
+#	| jq .success
 
